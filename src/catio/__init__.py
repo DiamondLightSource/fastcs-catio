9ed36014e092a8c0c74972a10af2b14efa3de771
Giles Knap <giles.knap@diamond.ac.uk>
2026-01-12 10:41:46 +0000

    initial conversion to copier template 5.0.1

--- src/catio/__init__.py
+++ src/catio/__init__.py
@@ -1,6 +1,11 @@
-from importlib.metadata import version  # noqa
+"""Top level API.
 
-__version__ = version("CATio")
-del version
+.. data:: __version__
+    :type: str
+
+    Version number as calculated by https://github.com/pypa/setuptools_scm
+"""
+
+from ._version import __version__
 
 __all__ = ["__version__"]
